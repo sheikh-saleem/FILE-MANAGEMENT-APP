4a1c061944eacc32c16b22a82de8897a39bd3efd
saleem ullah <156116341+sheikh-saleem@users.noreply.github.com>
2024-11-22 00:15:05 +0500

    Add files via upload
    This is simple program to READ,WRITE,EDIT,DELETE,CREATE,file

---
+++ read.py
@@ -0,0 +1,4 @@
+file=open("sample.txt","r")
+content=file.readline()
+print(content)
+file.close()
---
+++ write.py
@@ -0,0 +1,3 @@
+file=open("sample.txt","w")
+file.write("Hyee..! This is new python content ")
+file.close
